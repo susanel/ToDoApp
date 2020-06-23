808f61d662ce33d5b38415bfda36108a34591186
Susanel92 <zuzannanizka@gmail.com>
2020-06-23 23:51:18 +0200

    add functions generating modal

--- main.js
+++ main.js
@@ -1,174 +1,194 @@
 // Picking up elements
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 const inputAddTask = document.querySelector('input[data-name="add-input"');
-const inputSearchTask = document.querySelector('input[data-name="search-input"');
+const inputSearchTask = document.querySelector(
+  'input[data-name="search-input"'
+);
 const ulPending = document.querySelector('ul[data-list="to-do"]');
 const ulCompleted = document.querySelector('ul[data-list="done"]');
-
-const spanTasksNumber = document.querySelector('.count-tasks span');
-const showAddInputBtn = document.querySelector('.fa-plus');
-const showSearchInputBtn = document.querySelector('.fa-search');
-const showTasksBtns = document.querySelectorAll('.show-tasks [data-btn]');
+const spanTasksNumber = document.querySelector(".count-tasks span");
+const showAddInputBtn = document.querySelector(".fa-plus");
+const showSearchInputBtn = document.querySelector(".fa-search");
+const showTasksBtns = document.querySelectorAll(".show-tasks [data-btn]");
+// const modal = document.querySelector(".modal");
 
 const pendingTasksArray = [];
 const completedTasksArray = [];
 
-
 // FUNCTIONS
+const showPieChart = (per) => {
+  const chart = document.querySelector(".chart");
+  chart.style.background = `conic-gradient(salmon 0% ${per}%,orange ${per}% 100%)`;
+};
+
+const countPercentage = () => {
+  const allTasksNr = pendingTasksArray.length + completedTasksArray.length;
+  const completedTasksNr = completedTasksArray.length;
+
+  let percent = (completedTasksNr / allTasksNr) * 100;
+  percent = percent.toFixed(1);
+  showPieChart(percent);
+};
+
+const generateModal = () => {
+  console.log("klik");
+  document.querySelector(".modal-wrap").classList.add("active");
+  document.querySelector(".wrap").classList.add("blur");
+  countPercentage();
+};
 
 const showTasksList = (e) => {
-
   const btn = e.target;
 
   //reset
-  ulPending.textContent = '';
-  ulCompleted.textContent = '';
+  ulPending.textContent = "";
+  ulCompleted.textContent = "";
 
   //render suitable list
-  if (btn.dataset.btn === 'btn-all') {
+  if (btn.dataset.btn === "btn-all") {
     renderTasksList();
     renderCompletedTasksList();
-  } else if (btn.dataset.btn === 'btn-active') {
+  } else if (btn.dataset.btn === "btn-active") {
     renderTasksList();
-  } else if (btn.dataset.btn === 'btn-completed') {
+  } else if (btn.dataset.btn === "btn-completed") {
     renderCompletedTasksList();
   }
-}
+};
 
 const showInput = (e) => {
-  const addInputWrap = document.querySelector('.input-wrap:nth-child(1)');
-  const searchInputWrap = document.querySelector('.input-wrap:nth-child(2)');
+  const addInputWrap = document.querySelector(".input-wrap:nth-child(1)");
+  const searchInputWrap = document.querySelector(".input-wrap:nth-child(2)");
   const btn = e.target;
 
-  e.target.classList.toggle('active')
+  e.target.classList.toggle("active");
 
-  if (btn.classList.contains('fa-plus')) {
-
+  if (btn.classList.contains("fa-plus")) {
     //if inputSearchTask is visible, replace it with search input
-    if (searchInputWrap.classList.contains('on')) {
-      searchInputWrap.classList.toggle('on');
-      showSearchInputBtn.classList.toggle('active');
+    if (searchInputWrap.classList.contains("on")) {
+      searchInputWrap.classList.toggle("on");
+      showSearchInputBtn.classList.toggle("active");
     }
-    addInputWrap.classList.toggle('on')
-
-  } else if (btn.classList.contains('fa-search')) {
-
+    addInputWrap.classList.toggle("on");
+  } else if (btn.classList.contains("fa-search")) {
     //if inputAddTask is visible, replace it with search input
-    if (addInputWrap.classList.contains('on')) {
-      addInputWrap.classList.toggle('on');
-      showAddInputBtn.classList.toggle('active');
+    if (addInputWrap.classList.contains("on")) {
+      addInputWrap.classList.toggle("on");
+      showAddInputBtn.classList.toggle("active");
     }
-    searchInputWrap.classList.toggle('on')
+    searchInputWrap.classList.toggle("on");
   }
-}
+};
 
 const searchTask = (e) => {
-
   const searchText = e.target.value.toLowerCase();
 
   //search in pending Tasks
-  const tasks = pendingTasksArray.filter(li => li.textContent.toLowerCase().includes(searchText));
-  ulPending.textContent = '';
-  tasks.forEach(li => ulPending.appendChild(li));
+  const tasks = pendingTasksArray.filter((li) =>
+    li.textContent.toLowerCase().includes(searchText)
+  );
+  ulPending.textContent = "";
+  tasks.forEach((li) => ulPending.appendChild(li));
 
   //search in completed tasks
-  const tasksCompleted = completedTasksArray.filter(li => li.textContent.toLowerCase().includes(searchText));
-  ulCompleted.textContent = '';
-  tasksCompleted.forEach(li => ulCompleted.appendChild(li));
-
-}
+  const tasksCompleted = completedTasksArray.filter((li) =>
+    li.textContent.toLowerCase().includes(searchText)
+  );
+  ulCompleted.textContent = "";
+  tasksCompleted.forEach((li) => ulCompleted.appendChild(li));
+};
 
 const countTasks = () => {
   spanTasksNumber.textContent = pendingTasksArray.length;
-}
+};
 
 const renderTasksList = () => {
   ulPending.textContent = "";
   pendingTasksArray.forEach((task, key) => {
     task.dataset.key = key;
     ulPending.appendChild(task);
-  })
-}
+  });
+};
 
 const renderCompletedTasksList = () => {
   ulCompleted.textContent = "";
   completedTasksArray.forEach((task, key) => {
     task.dataset.key = key;
     ulCompleted.appendChild(task);
-  })
-}
+  });
+};
 
 const removeTask = (e) => {
   const index = e.target.parentNode.dataset.key;
-  if (e.target.parentNode.classList.contains('ongoing')) {
+  if (e.target.parentNode.classList.contains("ongoing")) {
     // console.log('ongoing');
     pendingTasksArray.splice(index, 1);
     renderTasksList();
-  } else if (e.target.parentNode.classList.contains('completed')) {
+  } else if (e.target.parentNode.classList.contains("completed")) {
     // console.log('completed');
     completedTasksArray.splice(index, 1);
     renderCompletedTasksList();
   }
   countTasks();
-}
+};
 
 const addCompletedTask = (e) => {
   // console.log(e.target); //i
   const completedTask = e.target.parentNode; //li
   const index = e.target.parentNode.dataset.key;
 
-  completedTask.classList.toggle('ongoing');
-  completedTask.classList.toggle('completed');
+  completedTask.classList.toggle("ongoing");
+  completedTask.classList.toggle("completed");
 
-  if (completedTask.classList.contains('completed')) {
+  if (completedTask.classList.contains("completed")) {
     // console.log('task completed');
 
     //changes icon to .fa-check-square
     e.target.outerHTML = '<i class="fas fa-check-square"></i>';
-    completedTask.querySelector('.fa-check-square').addEventListener('click', addCompletedTask);
+    completedTask
+      .querySelector(".fa-check-square")
+      .addEventListener("click", addCompletedTask);
 
     //remove completed task from pendingTasksArray and ulPending
     pendingTasksArray.splice(index, 1);
     renderTasksList();
-    countTasks()
+    countTasks();
 
     //update completedTaskArray with new finished task
     completedTasksArray.push(completedTask);
 
     //render list of completed tasks
-    renderCompletedTasksList()
-
-  } else if (completedTask.classList.contains('ongoing')) {
-
+    renderCompletedTasksList();
+  } else if (completedTask.classList.contains("ongoing")) {
     //changes icon to .fa-square
     e.target.outerHTML = '<i class="far fa-square"></i>';
-    completedTask.querySelector('.fa-square').addEventListener('click', addCompletedTask);
+    completedTask
+      .querySelector(".fa-square")
+      .addEventListener("click", addCompletedTask);
 
     //remove task from completedTaskList and ulCompleted
     completedTasksArray.splice(index, 1);
-    renderCompletedTasksList()
+    renderCompletedTasksList();
 
     //add task back to ToDoList
     pendingTasksArray.push(completedTask);
     renderTasksList();
     countTasks();
   }
-}
+};
 
 const addNewTask = (e) => {
-
   // prevents from page reloading
-  e.preventDefault()
+  e.preventDefault();
 
   const inputText = inputAddTask.value;
 
   // check if user-value is empty
-  if (inputText === "") return
+  if (inputText === "") return;
 
   // Add new element
-  const task = document.createElement('li');
-  task.classList.add('task');
-  task.classList.add('ongoing');
+  const task = document.createElement("li");
+  task.classList.add("task");
+  task.classList.add("ongoing");
   // task.dataset.
   task.innerHTML = `<i class="far fa-square"></i>
   <p>${inputText}</p>
@@ -181,45 +201,55 @@ const addNewTask = (e) => {
   renderTasksList();
 
   //clear input textarea
-  inputAddTask.value = '';
+  inputAddTask.value = "";
 
   // count tasks
   countTasks();
 
   //add eventListener to buttons of new elements
-  task.querySelector('.fa-times').addEventListener('click', removeTask);
-  task.querySelector('.fa-square').addEventListener('click', addCompletedTask);
-}
+  task.querySelector(".fa-times").addEventListener("click", removeTask);
+  task.querySelector(".fa-square").addEventListener("click", addCompletedTask);
+};
 
 const renderExampleTasks = () => {
-  const exampleTexts = ['learn React', 'learn SASS', 'learn Node.js'];
+  const exampleTexts = ["learn React", "learn SASS", "learn Node.js"];
   for (let i = 0; i < exampleTexts.length; i++) {
-    let exampleTask = document.createElement('li');
-    exampleTask.classList.add('task');
-    exampleTask.classList.add('ongoing')
+    let exampleTask = document.createElement("li");
+    exampleTask.classList.add("task");
+    exampleTask.classList.add("ongoing");
     exampleTask.innerHTML = `<i class="far fa-square"></i>
     <p>${exampleTexts[i]}</p>
     <i class="fas fa-times"></i>`;
-    pendingTasksArray.push(exampleTask)
+    pendingTasksArray.push(exampleTask);
 
     //add eventListener to buttons of example elements
-    exampleTask.querySelector('.fa-times').addEventListener('click', removeTask);
-    exampleTask.querySelector('.fa-square').addEventListener('click', addCompletedTask);
+    exampleTask
+      .querySelector(".fa-times")
+      .addEventListener("click", removeTask);
+    exampleTask
+      .querySelector(".fa-square")
+      .addEventListener("click", addCompletedTask);
   }
-  renderTasksList()
-  countTasks()
-}
+  renderTasksList();
+  countTasks();
+};
 
-renderExampleTasks()
-
+renderExampleTasks();
 
 // ADD EVENT LISTENERS
 
-form.addEventListener('submit', addNewTask);
+form.addEventListener("submit", addNewTask);
 
-inputSearchTask.addEventListener('input', searchTask);
+inputSearchTask.addEventListener("input", searchTask);
 
-showAddInputBtn.addEventListener('click', showInput);
-showSearchInputBtn.addEventListener('click', showInput);
+showAddInputBtn.addEventListener("click", showInput);
+showSearchInputBtn.addEventListener("click", showInput);
 
-showTasksBtns.forEach(btn => btn.addEventListener('click', showTasksList));
+showTasksBtns.forEach((btn) => btn.addEventListener("click", showTasksList));
+
+document.querySelector("button.stats").addEventListener("click", generateModal);
+
+document.querySelector("div.hide").addEventListener("click", () => {
+  document.querySelector(".modal-wrap").classList.remove("active");
+  document.querySelector(".wrap").classList.remove("blur");
+});
